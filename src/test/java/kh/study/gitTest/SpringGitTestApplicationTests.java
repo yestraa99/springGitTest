5412861229b1999049e06493191ba069369246b7
yestraa99 <yestraa@naver.com>
2022-10-26 14:14:50 +0900

    프로젝트 생성

---
+++ src/test/java/kh/study/gitTest/SpringGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.study.gitTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
